4d73fbf5cffd26b19fa89569e441da0c500d45c6
ultra-slimer <ezeoyhambure@gmail.com>
2020-03-30 12:54:08 -0300

    Oh captain, my captain

---
+++ app/src/main/java/com/example/tp1_oyhambure/Part2Activity.java
@@ -0,0 +1,17 @@
+package com.example.tp1_oyhambure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Part2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part2);
+    }
+}
